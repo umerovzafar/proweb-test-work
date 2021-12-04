379270ad43c9d542f6017815899311c4ea2bb372
umerovzafar <umerov.z99@gmail.com>
2021-12-04 18:20:31 +0500

    Незначительные внешние изменения

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
-    var sliderKey = 0;
-    var slides = document.querySelectorAll(".slide__item");
-    var slides_image = document.querySelectorAll(".slide__content");
-    var nextButton = document.getElementById("next-btn")
-    var lastButton = document.getElementById("last-btn")
+    let sliderKey = 0;
+    let slides = document.querySelectorAll(".slide__item");
+    let slides_image = document.querySelectorAll(".slide__content");
+    let nextButton = document.getElementById("next-btn")
+    let lastButton = document.getElementById("last-btn")
 
     function slider(reverse = false) {
         if (reverse && sliderKey === 0) {
@@ -14,13 +14,13 @@ window.addEventListener("DOMContentLoaded", () => {
         } else {
             reverse ? --sliderKey : sliderKey++
         }
-        for (var i = 0; i < slides.length; i++) {
+        for (let i = 0; i < slides.length; i++) {
             slides[i].classList.add("d-none");
             if (sliderKey === i) {
                 slides[i].classList.remove("d-none");
             }
         }
-        for (var j = 0; j < slides_image.length; j++) {
+        for (let j = 0; j < slides_image.length; j++) {
             slides_image[j].classList.add("d-none");
             if (sliderKey === j) {
                 slides_image[j].classList.remove("d-none");
